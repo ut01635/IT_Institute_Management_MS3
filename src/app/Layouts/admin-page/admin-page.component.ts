05d1fce771caa116accf946db715f6ad2e5b4877
ut01635 <ut01635tic@gmail.com>
2024-11-19 17:00:05 +0530

    update admin page

--- src/app/Layouts/admin-page/admin-page.component.ts
+++ src/app/Layouts/admin-page/admin-page.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
+  title = 'coding-academy';
+  isSidebarVisible = false; // Default is sidebar hidden on small screens
 
+  greeting: string = '';
+
+  // Function to toggle the sidebar visibility
+  toggleSidebar() {
+    this.isSidebarVisible = true;
+  }
+
+  constructor() {
+    this.setGreeting();
+  }
+
+  setGreeting() {
+    const hours = new Date().getHours();
+    if (hours >= 5 && hours < 12) {
+      this.greeting = 'Hello admin, Good Morning';
+    } else if (hours >= 12 && hours < 18) {
+      this.greeting = 'Hello admin, Good Afternoon';
+    } else {
+      this.greeting = 'Hello admin, Good Evening';
+    }
+  }
 }
