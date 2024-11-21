645d6a6325f9e6df4b3bd6be6d999c0fbc6a12cb
PPPiragash <pppiragash2004@gmail.com>
2024-11-21 15:28:51 +0530

    Craete Sudent Service Call Api Get All Students and Get Data In Student List

--- src/app/Services/enrollment.service.ts
+++ src/app/Services/enrollment.service.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,18 @@ import { Injectable } from '@angular/core';
 })
 export class EnrollmentService {
 
-  constructor() { }
+  private enrollmentUrl = 'https://localhost:7055/api/Enrollment';
+  private completedEnrollmentUrl = 'https://localhost:7055/api/Enrollment/all/completed';
+
+  constructor(private http: HttpClient) { }
+
+  
+  getEnrollments(studentNic: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.enrollmentUrl}?studentNic=${studentNic}`);
+  }
+
+  
+  getCompletedEnrollments(studentNic: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.completedEnrollmentUrl}?studentNic=${studentNic}`);
+  }
 }
