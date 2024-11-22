9c3a39a2ca3788ffe4c9c946cdf39efaa47e2ed9
PPPiragash <pppiragash2004@gmail.com>
2024-11-22 09:41:24 +0530

    Show Data in Course in Course List Compoents

--- src/app/Components/admin/course-list/course-list.component.ts
+++ src/app/Components/admin/course-list/course-list.component.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Course } from '../../../Services/Modal';
+import { CourseService } from '../../../Services/course.service';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
   searchText: string = '';
+  courses: Course[] = [];
 
-  courses: Course[] =[];
+  constructor(private courseService: CourseService) { }
 
-
-  onDelete(arg0: any) {
-    throw new Error('Method not implemented.');
+  ngOnInit(): void {
+    this.loadCourses();
   }
 
-  
-  onEdit(arg0: any) {
-    throw new Error('Method not implemented.');
+  loadCourses() {
+    this.courseService.getAllCourses().subscribe(data => {
+      this.courses = data;
+    });
   }
 
+  onEdit(courseId: string) {
+    console.log("Edit course with ID: ", courseId);
+  }
+
+  onDelete(courseId: string) {
+    console.log("Delete course with ID: ", courseId);
+  }
 
 }
