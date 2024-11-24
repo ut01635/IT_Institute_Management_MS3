f8d4714f0f71db3f657ff850f786ef7a82fd9c7a
ut01635 <ut01635tic@gmail.com>
2024-11-25 00:01:11 +0530

    uploaded payment plan form and admin dash boards

--- src/app/Components/student/course-view/course-view.component.ts
+++ src/app/Components/student/course-view/course-view.component.ts
@@ -14,7 +14,7 @@ export class CourseViewComponent implements OnInit {
   greeting:string = ''
   adminName:string = "User"
   // Reactive form for payment plan
-  paymentForm: FormGroup | undefined;
+  paymentForm!: FormGroup ;
   selectedCourse: any;
   
   constructor(private greetinService : GreetingService, private courseService:CourseService, private fb: FormBuilder){}
@@ -34,61 +34,20 @@ export class CourseViewComponent implements OnInit {
     });
   }
   // Array of course objects to be rendered dynamically
-  courses:Course[] = [
-    // {
-    //   title: 'HTML',
-    //   subtitle: 'Basic HTML5',
-    //   level: 'Beginner',
-    //   duration: '3 months',
-    //   description: 'This Intermediate-level HTML course at DevHub Institute is designed for individuals new to web development.',
-    //   price: 'LKR 15000.00',
-    //   imageUrl: '/html.jpg',
-    //   border: false
-    // },
-    // {
-    //   title: 'CSS',
-    //   subtitle: 'Advanced CSS',
-    //   level: 'Intermediate',
-    //   duration: '4 months',
-    //   description: 'This course is designed for those who already have a basic understanding of CSS and want to enhance their skills.',
-    //   price: 'LKR 20000.00',
-    //   imageUrl: '/html.jpg',
-    //   border: true
-    // },
-    // {
-    //   title: 'JavaScript',
-    //   subtitle: 'JavaScript for Beginners',
-    //   level: 'Beginner',
-    //   duration: '2 months',
-    //   description: 'Learn JavaScript from scratch and build interactive web applications.',
-    //   price: 'LKR 18000.00',
-    //   imageUrl: '/html.jpg',
-    //   border: false
-    // }
-    // Add more courses as needed
-  ];
+  courses:Course[] = [];
 
-  handleProfile(course: any) {
-    // Handle profile view action here
-    console.log('Viewing profile for: ', course.title);
-  }
-
-  handleFollow(course: any) {
-    // Handle follow action here
-    console.log('Following course: ', course.title);
-  }
 
    
  
    
  
   
-   // Submit Payment Plan
-  //  submitPaymentPlan(): void {
-  //    if (this.paymentForm.valid) {
-  //      const selectedPlan = this.paymentForm.get('paymentPlan')?.value;
-  //      alert(`You have selected the ${selectedPlan} payment plan for the course ${this.selectedCourse.courseName}`);
-  //      this.paymentForm.reset();
-  //    }
-  //  }
+  //  Submit Payment Plan
+   submitPaymentPlan(): void {
+     if (this.paymentForm.valid) {
+       const selectedPlan = this.paymentForm.get('paymentPlan')?.value;
+       alert(`You have selected the ${selectedPlan} payment plan for the course ${this.selectedCourse.courseName}`);
+       this.paymentForm.reset();
+     }
+   }
 }
