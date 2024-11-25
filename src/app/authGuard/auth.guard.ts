71f91be270228729da4c4280236d14fac47c5cdd
ut01635 <ut01635tic@gmail.com>
2024-11-26 02:00:39 +0530

    update auth guard

--- src/app/authGuard/auth.guard.ts
+++ src/app/authGuard/auth.guard.ts
@@ -7,26 +7,15 @@ import { Injectable } from '@angular/core';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
-    // Get the required role from the route data
-    const requiredRole = route.data['role'];
-
-    // Check if the user is logged in
-    if (this.authService.isLoggedIn()) {
-      // Check if the user's role matches the required role for this route
-      const userRole = localStorage.getItem('Role'); // Get role from localStorage
-
-      if (userRole && userRole === requiredRole) {
-        return true; // Allow access
-      } else {
-        this.router.navigate(['/']); // Redirect if the role doesn't match
-        return false;
-      }
-    } else {
-      this.router.navigate(['/']); // Redirect to login if not authenticated
-      return false;
-    }
+  constructor(private userService: AuthService, private router: Router) {
   }
+  canActivate(): boolean {
+	if (this.userService.isLoggedIn()) {
+		return true;
+	} else {
+		this.router.navigate(['/']);
+		return false;
+       }
+    }
 }
