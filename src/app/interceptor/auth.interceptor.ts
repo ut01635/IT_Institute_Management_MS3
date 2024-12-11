153590fdd73eaf46cd6109f465837a7cb5ef4313
PPPiragash <pppiragash2004@gmail.com>
2024-12-11 09:10:10 +0530

    Auth interceptor Successfully Worked

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Router } from '@angular/router'; // For navigation to login page
+import { Router } from '@angular/router'; 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private alertShown: boolean = false; // Flag to track if the alert has already been shown
+  private alertShown: boolean = false; 
 
   constructor(private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('Token'); // Get token from localStorage
+    const token = localStorage.getItem('Token'); 
     let clonedRequest = req;
 
-    // Add Authorization header if token exists
+    
     if (token) {
       clonedRequest = req.clone({
         setHeaders: {
@@ -25,25 +25,24 @@ export class AuthInterceptor implements HttpInterceptor {
 
     return next.handle(clonedRequest).pipe(
       catchError((error: HttpErrorResponse) => {
-        // console.log("aa" + error.message);
-        // console.log("bb" + error.status);
+      
         
-        if (error.status === 0 && !this.alertShown) {  // Check if alert is not already shown
+        if (error.status === 0 && !this.alertShown) {  
           console.log("hello this error"+error.error);
           
-          // Remove token from localStorage (logout)
+          
           localStorage.clear();
           
-          // If session expired or unauthorized, log out the user
-          this.alertShown = true;  // Set the flag to true to prevent future alerts
+        
+          this.alertShown = true;  
           alert('Session expired. Please log in again.');
           
-          // Navigate to the login page or home page
-          this.router.navigate(['/']);  // Adjust the URL to where the login page is
+         
+          this.router.navigate(['/']);  
           // this.alertShown = false;
         }
 
-        // Re-throw the error so it can be handled elsewhere if needed
+       
         return throwError(() => error);
       })
     );
